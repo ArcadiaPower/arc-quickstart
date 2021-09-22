c096cc0379a4013b015ad086d1aab4a6aa24b4f0
Matthew Gerring <gerring.matthew@gmail.com>
2021-09-22 16:04:44 -0700

    Hook up the router and add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ConnectUtilityAccount from './components/ConnectUtilityAccount.jsx';
 import SelectCarbonOffsetProject from './components/SelectCarbonOffsetProject.jsx';
+import CarbonOffsetProjectDetail from './components/CarbonOffsetProjectDetail.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
         <Route exact path="/" component={ConnectUtilityAccount} />
         <Route exact path="/select" component={SelectCarbonOffsetProject} />
+        <Route exact path="/select/:projectType" component={CarbonOffsetProjectDetail} />
     </Switch>
   </BrowserRouter>
 );
