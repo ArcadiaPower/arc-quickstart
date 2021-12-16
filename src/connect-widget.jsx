f6b33b7d136c1a6a6530960c09955e2b3c6bf97c
Clif Reeder <clif.reeder@arcadia.com>
2021-12-16 15:23:11 -0700

    Update src/connect-widget.jsx
    Co-authored-by: Chelsea Sandridge <chelsea.sandridge@gmail.com>

--- src/connect-widget.jsx
+++ src/connect-widget.jsx
@@ -36,7 +36,6 @@ const ConnectWidget = () => {
   const generateConfig = connectToken => {
     return {
       connectToken,
-      env: 'production',
       callbacks: {
         // Called each time the Connect Component is opened.
         onOpen: () => {
