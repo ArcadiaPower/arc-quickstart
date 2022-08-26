d7ad2b7f348dae354e52d0f92957e0c39f3b7717
xelzi <chelsea.sandridge@gmail.com>
2022-08-26 12:27:54 -0400

    yay an actual 'working' modal

--- src/components/utility-statement-element.jsx
+++ src/components/utility-statement-element.jsx
@@ -2,14 +2,30 @@ import { useState } from 'react';
 import JSONPretty from 'react-json-pretty';
 import { calculateCounterfactualBill } from "../session.js";
 import { object } from 'prop-types';
+import Modal from 'react-modal';
 import CalculationModal from './calculation-modal.jsx'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 const UtilityStatementElement = ({arcUtilityStatement}) => {
   const [openModal, setOpenModal] = useState(false)
 
   const calculate = async (arcUtilityStatementId) => {
-    const result = await calculateCounterfactualBill(arcUtilityStatementId)
     setOpenModal(true)
+    const result = await calculateCounterfactualBill(arcUtilityStatementId)
+  }
+
+  const closeModal = () => {
+    setOpenModal(false)
   }
 
   return (
@@ -18,7 +34,10 @@ const UtilityStatementElement = ({arcUtilityStatement}) => {
       <button onClick={() => calculate(arcUtilityStatement.id)}>
         Calculate Counterfactual Bill for Arc Utility Statement {arcUtilityStatement.id}
       </button>
-      {openModal && <CalculationModal setOpenModal={setOpenModal}/>}
+      <Modal isOpen={openModal} style={customStyles}>
+        <p> We will display the results here if the exist and present loading if they are loading</p>
+        <button onClick={closeModal}>close</button>
+      </Modal>
     </div>
   )
 };
